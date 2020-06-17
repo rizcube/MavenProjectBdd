11641d551990e30c4ede3e4989faeb95114ba230
rizcube <rizcubeservices@gmail.com>
2020-06-17 17:34:44 +0100

    first commit

---
+++ src/main/java/BDD_/apiFramework_/App.java
@@ -0,0 +1,13 @@
+package BDD_.apiFramework_;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
