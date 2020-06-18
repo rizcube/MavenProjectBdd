42e370444f77e9a03d7f6b2ed2997463437003a9
rizcube <rizcubeservices@gmail.com>
2020-06-18 09:02:19 +0100

    copied given code from SpecBuilderTest created earlier, created a new package called pojo and moved two files AddPlace.java and Location.java created earlier when creating pojo classes

--- src/test/java/stepDefinitions/stepDefinition.java
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -1,15 +1,59 @@
 package stepDefinitions;
 
+import static io.restassured.RestAssured.given;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import pojo.AddPlace;
+import pojo.Location;
 
 public class stepDefinition {
 
 	@Given("Add Place Payload")
 	public void add_Place_Payload() {
 	    // Write code here that turns the phrase above into concrete actions
-	    System.out.println("addPlacepayload");
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		
+		AddPlace p = new AddPlace();
+		p.setAccuracy(50);
+		p.setName("Princess Castle 3");
+		p.setPhone_number("(+91) 983 893 3937");
+		p.setAddress("Spiritual Landing place");
+		p.setWebsite("https://www.google.com");
+		p.setLanguage("French-IN");
+		
+		
+	// types is a list so we have to create a list object first
+		List<String> myList = new ArrayList<String>();
+		myList.add("shoe park");
+		myList.add("shop");
+		p.setTypes(myList);
+		
+		Location l= new Location();
+		l.setLng(-38.383494);
+		l.setLng(33.427362);
+		
+		p.setLocation(l);
+		
+		// set is used for setting parameters
+		RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123")
+		.setContentType(ContentType.JSON).build();
+		
+		
+		ResponseSpecification resspec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
+		RequestSpecification res = given().spec(req)
+		.body(p);
+		
 	}
 	
 	@When("user calls {string} with Post http request")
